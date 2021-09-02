bde61fbb308520706ca7f5db04e7dba6488c551e
Stephen Mahon <stemahon@gmail.com>
2021-09-01 21:09:36 -0500

    Web Development with Go by Jon Calhoun 02

--- gowiki/wiki.go
+++ gowiki/wiki.go
@@ -1,5 +1,14 @@
 package main
 
+/*
+To do:
+	[ ] Store templates in tmpl/ and page data in data/.
+	[ ] Add a handler to make the web root redirect to /view/FrontPage.
+	[ ] Spruce up the page templates by making them valid HTML and adding some CSS rules.
+	[ ] Implement inter-page linking by converting instances of [PageName] to
+	    <a href="/view/PageName">PageName</a>. (hint: you could use regexp.ReplaceAllFunc to do this)
+*/
+
 import (
 	"errors"
 	"io/ioutil"
